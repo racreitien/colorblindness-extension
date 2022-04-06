60c215a9885e8bb1b04525a78f642da6934cea52
racreitien <rachelburns7@gmail.com>
2022-04-06 18:14:26 -0500

    get all elements in body and calculate contrast ratio

--- popup.js
+++ popup.js
@@ -2,9 +2,40 @@ let contrastSwitch = document.getElementById("contrastSwitch");
 
 import { pSBC, contrast } from './contrast.mjs';
 
-// TODO: find elements that have a bad contrast ratio according to WCAG AAA
-// 7:1 for text/background
-// 4.5:1 for heading or large text/background
+// find elements that have a bad contrast ratio according to WCAG AAA
+// 7:1 for regular text
+// 4.5:1 for heading or large text
+let elements = document.body.getElementsByTagName("*");  // get all elements inside body
+let badElements = [];
+
+for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    if (element.textContent.trim !== "") {
+        let textColor = element.style.color;
+        let backgroundColor = element.style.backgroundColor;
+
+        // calculate contrast ratio
+        let contrast = contrast(textColor, backgroundColor);
+
+        // find the needed contrast ratio depending on type of text
+        let goodContrast = NaN;
+
+        // define large text --> at least 18 pt regular font or 14 pt bold font
+        let fontWeight = getComputedStyle(element).fontWeight;
+        let largeFont = (Number(fontWeight) > 400 || fontWeight === "bold") ? 14 : 18;
+
+        if (element.nodeName.startsWith("H") || element.style.fontSize >= largeFont) {
+            goodContrast = 4.5;
+        }
+        else {
+            goodContrast = 7;
+        }
+
+        if (contrast < goodContrast) {
+            badElements.push(element);  // does not meet WCAG, need to fix
+        }
+    }
+}
 
 // TODO: for each pair, darken the darker color until AAA standard is met
 //       and apply the change to the HTML element
