c7f78083c297a615ce27e19b0f0a5c9e56e389e9
sms0325 <sms0325@tamu.edu>
2022-04-24 22:01:44 -0500

    pBSC much more readable

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ chrome.storage.sync.get("color", ({color}) => {
     btn.style.backgroundColor = color
 })
 
-import { pSBC, makeArrayRGB, realBackgroundColor, getContrast, minContrast, darken } from './contrast.mjs';
+// import { pSBC, makeArrayRGB, realBackgroundColor, getContrast, minContrast, darken } from './contrast.mjs';
 
 // listener for contrast switch being set to on/off
 btn.addEventListener("click", async () => {
@@ -29,11 +29,11 @@ btn.addEventListener("click", async () => {
             //from: "before" color, to: "after" color
             //percent: percentage to blend by (numerical value)
             //linear blending: boolean value for if you wish to use linear blending or not
-            let r, g, b, P, f, t, hex, m = Math.round, alpha = typeof(to) == "string";
+            let r, g, b, per, f, t, hex, alpha = typeof(to) == "string";
                 //r, g, b: RGB colors
+                //per: boolean to determine if your percentage is negative
                 //f, t: from/to values that are specified for the r, g, b colors
                 //hex: used for if you're dealing with a hex value
-                //m: for shortening the "Math.round" expression
                 //alpha: value for the alpha channel - used in linear blending
             if (typeof(percent) != "number" || percent < -1 || percent > 1 || typeof(from) != "string" || (from[0] != 'r' && from[0] != '#') || (to && !alpha)) {
                 return null;
@@ -41,30 +41,44 @@ btn.addEventListener("click", async () => {
             hex = from.length > 9;
             hex = alpha ? to.length > 9 ? true : to == "c" ? !hex : false : hex;
             f = pSBC.pSBCr(from);
-            P = percent < 0;
-            t = to && to != "c" ? pSBC.pSBCr(to) : P ? {r:0, g:0, b:0, alpha: -1} : {r:255, g:255, b:255, alpha: -1};
-            percent = P ? percent *- 1 : percent;
-            P = 1 - percent;
+            per = percent < 0; //boolean
+            t = to && to != "c" ? pSBC.pSBCr(to) : per ? {r:0, g:0, b:0, alpha: -1} : {r:255, g:255, b:255, alpha: -1};
+            percent = per ? percent *- 1 : percent; //if your percentage is negative, make it positive.
+            per = 1 - percent; //opposite of percentage
             if (!f || !t) { //if there's no 'from' or 'to' values to work with
                 return null;
             }
             if (linear_blending) {
-                r = m(P * f.r + percent * t.r);
-                g = m(P * f.g + percent * t.g);
-                b = m(P * f.b + percent * t.b);
+                r = Math.round(per * f.r + percent * t.r);
+                g = Math.round(per * f.g + percent * t.g);
+                b = Math.round(per * f.b + percent * t.b);
             } else {
-                r = m((P * f.r ** 2 + percent * t.r ** 2) ** 0.5);
-                g = m((P * f.g ** 2 + percent * t.g ** 2) ** 0.5);
-                b = m((P * f.b ** 2 + percent * t.b ** 2) ** 0.5);
+                r = Math.round((per * f.r ** 2 + percent * t.r ** 2) ** 0.5);
+                g = Math.round((per * f.g ** 2 + percent * t.g ** 2) ** 0.5);
+                b = Math.round((per * f.b ** 2 + percent * t.b ** 2) ** 0.5);
             }
-            alpha = f.alpha;
-            t = t.alpha;
-            f = alpha >= 0 || t >= 0;
-            alpha = f ? alpha < 0 ? t : t < 0 ? alpha : alpha * P + t * percent : 0;
-            if (hex) {
-                return "rgb(" + r + ", " + g + ", " + b + ")";
-            } else {
-                return "#" + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(alpha * 255) : 0)).toString(16).slice(1, f ? undefined : -2);
+            alpha = f.alpha; //from's alpha value
+            t = t.alpha; //to's alpha value
+            f = alpha >= 0 || t >= 0; //f is a boolean to determine if you have an alpha value, whether in a or in t.alpha
+            alpha = f ? alpha < 0 ? t : t < 0 ? alpha : alpha * per + t * percent : 0;
+            if (hex) { //hex to rgb conversion
+                let open_par = "";
+                let new_alpha = "";
+                if (f) {
+                    open_par = "a("; //if you've got an alpha value, include that in your string
+                    new_alpha = "," + Math.round(alpha * 1000)/1000; //re-calculate new alpha value
+                } else {
+                    open_par = "("; //no alpha value to start with
+                    new_alpha = ""; //no alpha value to add in.
+                }
+                return "rgb" + open_par + r + "," + g + "," + b + new_alpha + ")";
+            } else { //rgb to hex conversion
+                let new_alpha = 0;
+                let rgb_to_hex_formula = 4294967296 + r * 16777216 + g * 65536 + b * 256;
+                if (f) {
+                    new_alpha = Math.round(alpha * 255); //alpha value to put in, if you have an alpha.
+                } 
+                return "#" + (rgb_to_hex_formula + new_alpha).toString(16).slice(1, f ? undefined : -2);
             }
         }
 
@@ -72,34 +86,33 @@ btn.addEventListener("click", async () => {
         pSBC.pSBCr = (digits) => {
             //digits: what makes up the RGB value
             //r, g, b: RGB colors
-            const i = parseInt; //shorthand for parseInt
             let n = digits.length, x = {};
             if (n > 9) {
                 const [r, g, b, alpha] = (digits = digits.split(','));
                 n = digits.length;
-                if (n<3 || n>4) {
+                if (n < 3 || n > 4) { //digit length of 3
                     return null;
                 }
-                x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4));
-                x.g = i(g); 
-                x.b = i(b);
-                x.alpha = alpha ? parseFloat(alpha):-1;
+                x.r = parseInt(r[3] == "a" ? r.slice(5) : r.slice(4));
+                x.g = parseInt(g); 
+                x.b = parseInt(b);
+                x.alpha = alpha ? parseFloat(alpha) : -1;
             } else {
-                if (n == 8 || n == 6 || n<4) {
+                if (n == 8 || n == 6 || n < 4) {
                     return null;
                 }
                 if (n < 6) {
                     digits = "#" + digits[1] + digits[1] + digits[2] + digits[2] + digits[3] + digits[3] + (n > 4 ? digits[4] + digits[4] : "");
                 }
-                digits = i(digits.slice(1), 16);
+                digits = parseInt(digits.slice(1), 16);
                 if (n == 9 || n == 5) {
-                    x.r = digits >> 24&255;
-                    x.g = digits >> 16&255;
-                    x.b = digits >> 8&255;
+                    x.r = digits >> 24 & 255;
+                    x.g = digits >> 16 & 255;
+                    x.b = digits >> 8 & 255;
                     x.alpha = Math.round((digits & 255)/0.255)/1000;
                 } else {
                     x.r = digits >> 16;
-                    x.g = digits >> 8&255; 
+                    x.g = digits >> 8 & 255; 
                     x.b = digits & 255; 
                     x.alpha = -1;
                 }
@@ -133,19 +146,20 @@ btn.addEventListener("click", async () => {
 
         // calculates the contrast of two colors
         // origination: https://github.com/LeaVerou/contrast-ratio/tree/d402291022c882c9ae5547b755afa9976460374c
+        // relative luminance formula from W3: https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
         function luminance(r, g, b) {
             //r, g, b: RGB colors
             var a = [r, g, b].map(function (v) {         
                 v /= 255;
-                return v <= 0.03928
+                return v <= 0.03928 //relative luminance formula calculation
                     ? v / 12.92
                     : Math.pow( (v + 0.055) / 1.055, 2.4 );
             });
-            return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+            return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722; //calculating the luminance ratio
         }
 
         // origination: https://github.com/LeaVerou/contrast-ratio/tree/d402291022c882c9ae5547b755afa9976460374c
-        // calculate beginnning contrast
+        // calculate beginning contrast
         function getContrast(rgb1, rgb2) {
             var lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
             var lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
@@ -171,7 +185,7 @@ btn.addEventListener("click", async () => {
             return 7;  // normal text
         }
 
-        // TODO: add function that darkens a color to meet a certain ratio against a second color
+        // darkens a color to meet a certain ratio against a second color
         // getContrast():   [#,#,#] input
         // pSBC():          rgb(#,#,#) input
         function darken(darker, lighter, element) { //pass in element instead of contrast?
